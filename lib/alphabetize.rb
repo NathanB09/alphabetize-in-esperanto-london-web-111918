188d64da7fed442e34a3b06e9dcc62f1880af4b9
Nathan Bell <nathanbell09@gmail.com>
2018-11-13 16:49:19 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,6 @@
 def alphabetize(arr)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|phrase| phrase.tr(esperanto, alphabet)}
+  # arr.sort_by {|phrase| phrase.tr(esperanto, alphabet)}
+  arr.sort_by {|phrase| phrase.split.collect {|char| esperanto.index(char)}}
 end
