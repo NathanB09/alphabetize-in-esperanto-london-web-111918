52b9c815d0727d80e0823fb526b79afd3da66741
Nathan Bell <nathanbell09@gmail.com>
2018-11-13 16:59:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ require "pry"
 def alphabetize(arr)
   alphabet = "@-\\"
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|phrase| phrase.tr(esperanto, alphabet)}
+  sorted = arr.sort_by {|phrase| phrase.tr(esperanto, alphabet)}
   binding.pry
 end
 arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
